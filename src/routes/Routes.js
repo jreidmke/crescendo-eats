7480ab190ba3718d7d7c655bd5ed1171bd74f13e
James Reid <jreidmke@gmail.com>
2021-06-08 13:23:58 -0500

    init routing, add route for recipe detail view

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,1 +1,12 @@
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+import RecipeDetails from "../components/RecipeDetails";
+
+function Routes() {
+    <Switch>
+
+        <Route path="/recipes/:recipeId">
+            <RecipeDetails/>
+        </Route>
+
+    </Switch>
+}

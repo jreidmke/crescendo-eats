a3dd8585dadc6107e14961a31b01f8702f28ba23
James Reid <jreidmke@gmail.com>
2021-06-08 13:01:23 -0500

    init API, calls for GET all recipes, single recipe and all specials

---
+++ src/api/api.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:3001";
+
+class CrescendoEatsApi {
+
+    //Generic Request Method, Method Arg Defaults to "GET"
+
+    static async request(endpoint, data = {}, method = "get") {
+        console.debug("API Call To: ", endpoint, data, method);
+
+        const url = `${BASE_URL}/${endpoint}`;
+
+        const params = method === "get" ? data : {};
+
+        try {
+            return (await axios({ url, method, data, params })).data;
+          } catch (err) {
+            console.error("API Error:", err.response);
+            let message = err.response.data.error.message;
+            throw Array.isArray(message) ? message : [message];
+          }
+    };
+
+    //Individual API Routes
+
+    /** Get All Recipes */
+
+    static async getAllRecipes() {
+        let res = await this.request(`recipes`);
+        return res;
+    };
+
+    /**Get Recipe */
+
+    static async getRecipe(id) {
+        let res = await this.request(`recipes/${id}`);
+        return res;    
+    };
+
+
+
+    static async getSpecials() {
+        let res = await this.request(`specials`);
+        let ingredientIds = res.map(x => x.ingredientId)
+        return res;
+    }
+};
+
+export default CrescendoEatsApi;

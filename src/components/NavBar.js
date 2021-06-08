bb544695992fc940cbe6ed8f94b80b1a7621e2b9
James Reid <jreidmke@gmail.com>
2021-06-08 15:03:12 -0500

    completed first iteration of navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import {Link} from "react-router-dom";
 import "../styling/navbar.css";
+import { GrLinkedin, GrGithub } from 'react-icons/gr';
 
 function NavBar() {
     return(
@@ -8,10 +9,16 @@ function NavBar() {
                 <p id='title'>Crescendo Eats</p>
             </Link>
             <p id='tagline'>A place for techies to get some recipes.</p>
-
-            
         </nav>
     )
 };
 
 export default NavBar;
+
+
+{/* <ul className="nav navbar-nav navbar-right">
+<h4>
+    <a href="https://www.linkedin.com/in/jreidmke/"><GrLinkedin color="blue"/></a>
+    <a href="https://github.com/jreidmke"><GrGithub/></a>
+</h4>
+</ul> */}

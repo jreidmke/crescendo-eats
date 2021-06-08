f46e24413569f3ec2965b62990ee78017149aa23
James Reid <jreidmke@gmail.com>
2021-06-08 13:03:06 -0500

    init main react component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import CrescendoEatsApi from './api/api';
 import './App.css';
 
 function App() {
+
+  async function printAll() {
+    try {
+      let res = await CrescendoEatsApi.getAllRecipes();
+      console.log(res);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  async function printOne(id) {
+    try {
+      let res = await CrescendoEatsApi.getRecipe(id);
+      console.log(res);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function getSpecials() {
+    try {
+      let res = await CrescendoEatsApi.getSpecials();
+      console.log(res);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={printAll}>Print All</button>
+      <button onClick={() => printOne("e80ea521-4d42-48fe-a7a6-ac8952bc0de4")}>Print One</button>
+      <button onClick={getSpecials}>specials</button>
     </div>
   );
 }

abb81e3511f31dcf4d42a8c58674a920d6983319
James Reid <jreidmke@gmail.com>
2021-06-09 15:23:25 -0500

    added tests and fixed class of spinner

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,12 +1,88 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "../App";
+import Alert from "../components/common/Alert";
+import Footer from "../components/common/Footer";
+import NavBar from "../components/common/NavBar";
+import Spinner from "../components/common/Spinner";
+import { MemoryRouter } from "react-router";
 
 it("renders without crashing", async function() {
-    render(<App/>);
+    render(<MemoryRouter>
+                <App/>        
+            </MemoryRouter>
+    );
 });
   
 it("matches snapshot", () => {
-    const { asFragment } = render(<App/>);
+    const { asFragment } = render(
+        <MemoryRouter>
+            <App/>        
+        </MemoryRouter>        
+    );    
+    expect(asFragment()).toMatchSnapshot();
+});
+
+it("renders without crashing", async function() {
+    render(<MemoryRouter>
+                <Alert message="Error"/>        
+            </MemoryRouter>
+    );
+});
+  
+it("matches snapshot", () => {
+    const { asFragment } = render(
+        <MemoryRouter>
+            <Alert message="Error"/>        
+        </MemoryRouter>        
+    );    
+    expect(asFragment()).toMatchSnapshot();
+});
+
+it("renders without crashing", async function() {
+    render(<MemoryRouter>
+                <Footer/>        
+            </MemoryRouter>
+    );
+});
+  
+it("matches snapshot", () => {
+    const { asFragment } = render(
+        <MemoryRouter>
+            <Footer/>        
+        </MemoryRouter>        
+    );    
+    expect(asFragment()).toMatchSnapshot();
+});
+
+it("renders without crashing", async function() {
+    render(<MemoryRouter>
+                <NavBar/>        
+            </MemoryRouter>
+    );
+});
+  
+it("matches snapshot", () => {
+    const { asFragment } = render(
+        <MemoryRouter>
+            <NavBar/>        
+        </MemoryRouter>        
+    );    
+    expect(asFragment()).toMatchSnapshot();
+});
+
+it("renders without crashing", async function() {
+    render(<MemoryRouter>
+                <Spinner/>        
+            </MemoryRouter>
+    );
+});
+  
+it("matches snapshot", () => {
+    const { asFragment } = render(
+        <MemoryRouter>
+            <Spinner/>        
+        </MemoryRouter>        
+    );    
     expect(asFragment()).toMatchSnapshot();
 });
--- src/components/common/Spinner.js
+++ src/components/common/Spinner.js
@@ -1,7 +1,7 @@
 function Spinner() {
     return(
-        <div class="spinner-grow" role="status">
-            <span class="sr-only">Loading...</span>
+        <div className="spinner-grow" role="status">
+            <span className="sr-only">Loading...</span>
         </div>
     )
 };

6a6569657a60e4b90bb7afdadead7fd4e89d7c49
James Reid <jreidmke@gmail.com>
2021-06-09 07:03:58 -0500

    init forms for new and edit

---
+++ src/components/forms/NewRecipeForm.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import CrescendoEatsApi from "../../api/api";
+
+function NewRecipeForm() {
+    return(
+            <div className="container">
+                <div className="row">
+                    <h1>Add New Recipe</h1>
+                </div>
+
+                <div className="row">
+
+                </div>
+            </div>
+        )
+};
+
+export default NewRecipeForm;
+
+// "uuid": "e80ea521-4d42-48fe-a7a6-ac8952bc0de4",
+//     "title": "Queso Big Brat Scramble",
+//     "description": "A delicious breakfast, fit for a crowd.",
+//     "images": {
+//       "full": "/img/queso_brat_scramble.jpg",
+//       "medium": "/img/queso_brat_scramble--m.jpg",
+//       "small": "/img/queso_brat_scramble--s.jpg"
+//     },
+//     "servings": 5,
+//     "prepTime": 10,
+//     "cookTime": 20,

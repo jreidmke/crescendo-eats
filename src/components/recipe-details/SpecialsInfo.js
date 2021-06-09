a617246ec99dc398327ac4ec489b84a8c192afa2
James Reid <jreidmke@gmail.com>
2021-06-09 13:49:56 -0500

    new link for special edit form

--- src/components/recipe-details/SpecialsInfo.js
+++ src/components/recipe-details/SpecialsInfo.js
@@ -1,10 +1,12 @@
 import "../../styling/specials-box.css";
+import { FaEdit } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 function SpecialsInfo(id, specials) {
     for(let s of specials) {
         if(id === s.ingredientId) return(
             <div className="specials-box">
-                <h6>Special: {s.title.toUpperCase()} ({s.type})</h6>
+                <h6>Special: {s.title.toUpperCase()} ({s.type}) <Link to={`/specials/${s.uuid}/edit`}><FaEdit/></Link></h6>
                 <small>{s.text}</small><br/>
                 {"geo" in s ? <small><a href={`http://maps.google.com/maps?q=${s.geo}`}>Click here for location!</a></small> : ""}
             </div>

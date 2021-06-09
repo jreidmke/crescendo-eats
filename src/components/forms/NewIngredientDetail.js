51818a38f775a8514658607af0638963e90c1d54
James Reid <jreidmke@gmail.com>
2021-06-09 11:36:55 -0500

    clean up for readability

--- src/components/forms/NewIngredientDetail.js
+++ src/components/forms/NewIngredientDetail.js
@@ -1,9 +1,15 @@
 import "../../styling/ingredient-detail.css"
+import { BsTrash } from 'react-icons/bs';
 
 function NewIngredientDetail({ingredient}) {
+
     return(
         <div id="ingredient-detail">
-            <p><b>{ingredient.amount} {ingredient.measurement} {ingredient.measurement ? "of" : ""} {ingredient.name}</b></p>
+            <p>
+                <b>
+                    {ingredient.amount} {ingredient.measurement} {ingredient.measurement ? "of" : ""} {ingredient.name}
+                </b>
+            </p>
         </div>
     )
 }

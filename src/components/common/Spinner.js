a9b67edb838acdd04ab1f70ea8f55d5d7d55bf4a
James Reid <jreidmke@gmail.com>
2021-06-09 14:02:51 -0500

    added loading spinner

---
+++ src/components/common/Spinner.js
@@ -0,0 +1,9 @@
+function Spinner() {
+    return(
+        <div class="spinner-grow" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    )
+};
+
+export default Spinner;

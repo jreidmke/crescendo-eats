3a1615822f26923414d1065e88807c2466a24a1e
James Reid <jreidmke@gmail.com>
2021-06-09 08:57:42 -0500

    new component to display ingredients on recipe form

---
+++ src/components/forms/NewIngredientDetail.js
@@ -0,0 +1,11 @@
+import "../../styling/ingredient-detail.css"
+
+function NewIngredientDetail({ingredient}) {
+    return(
+        <div id="ingredient-detail">
+            <p><b>{ingredient.amount} {ingredient.measurement} {ingredient.measurement ? "of" : ""} {ingredient.name}</b></p>
+        </div>
+    )
+}
+
+export default NewIngredientDetail;

0b3b5b9c535238e0d419f1cd6959e0624ae9e3c7
James Reid <jreidmke@gmail.com>
2021-06-09 06:28:39 -0500

    new styling for navbar and specials details

--- src/components/SpecialsInfo.js
+++ src/components/SpecialsInfo.js
@@ -4,7 +4,7 @@ function SpecialsInfo(id, specials) {
     for(let s of specials) {
         if(id === s.ingredientId) return(
             <div className="specials-box">
-                <h6>{s.title.toUpperCase()} ({s.type})</h6>
+                <h6>Special: {s.title.toUpperCase()} ({s.type})</h6>
                 <small>{s.text}</small><br/>
                 {"geo" in s ? <small><a href={`http://maps.google.com/maps?q=${s.geo}`}>Click here for location!</a></small> : ""}
             </div>

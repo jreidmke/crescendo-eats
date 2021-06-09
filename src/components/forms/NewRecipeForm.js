e11565a87445ed11788c3bcb808c4530bf9d5d10
James Reid <jreidmke@gmail.com>
2021-06-09 08:57:14 -0500

    begin styling new recipe form

--- src/components/forms/NewRecipeForm.js
+++ src/components/forms/NewRecipeForm.js
@@ -2,6 +2,9 @@ import CrescendoEatsApi from "../../api/api";
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { useHistory } from 'react-router-dom';
+import NewIngredientDetail from "./NewIngredientDetail";
+
+import "../../styling/new-form.css";
 
 function NewRecipeForm() {
     const history = useHistory();
@@ -13,7 +16,7 @@ function NewRecipeForm() {
     };
 
     const instructionDataDefault = {
-        instruction: "",
+        instructions: "",
         optional: ""
     }
 
@@ -68,7 +71,10 @@ function NewRecipeForm() {
 
     async function submit(e) {
         e.preventDefault();
-        await CrescendoEatsApi.newRecipe(formData);
+        let newRecipe = formData;
+        newRecipe.ingredients = ingredientList;
+        newRecipe.directions = instructionList;
+        await CrescendoEatsApi.newRecipe(newRecipe);
         history.push('/');
     }
 
@@ -80,119 +86,121 @@ function NewRecipeForm() {
                 </div>
 
                 <div className="row">
-                    <form onSubmit={submit}>
+                    <div className="col-4">
+                        <form onSubmit={submit}>
 
-                        <input
-                            type="text"
-                            name="title"
-                            onChange={handleChange}
-                            value={formData.title}
-                            placeholder="Dish Name"
-                            className="form-control"
-                            required/>
+                            <input
+                                type="text"
+                                name="title"
+                                onChange={handleChange}
+                                value={formData.title}
+                                placeholder="Dish Name"
+                                className="form-control"
+                                required/>
 
-                        <input
-                            type="text"
-                            name="description"
-                            onChange={handleChange}
-                            value={formData.description}
-                            placeholder="Dish Description"
-                            className="form-control"
-                            required/>
+                            <input
+                                type="text"
+                                name="description"
+                                onChange={handleChange}
+                                value={formData.description}
+                                placeholder="Dish Description"
+                                className="form-control"
+                                required/>
 
-                        <input
-                            type="number"
-                            name="servings"
-                            onChange={handleChange}
-                            value={formData.servings}
-                            placeholder="Servings"
-                            className="form-control"
-                            required/>
+                            <input
+                                type="number"
+                                name="servings"
+                                onChange={handleChange}
+                                value={formData.servings}
+                                placeholder="Servings"
+                                className="form-control"
+                                required/>
 
-                        <input
-                            type="number"
-                            name="prepTime"
-                            onChange={handleChange}
-                            value={formData.prepTime}
-                            placeholder="Prep Time in Minutes"
-                            className="form-control"
-                            required/>   
+                            <input
+                                type="number"
+                                name="prepTime"
+                                onChange={handleChange}
+                                value={formData.prepTime}
+                                placeholder="Prep Time in Minutes"
+                                className="form-control"
+                                required/>   
 
-                        <input
-                            type="number"
-                            name="cookTime"
-                            onChange={handleChange}
-                            value={formData.cookTime}
-                            placeholder="Cook Time in Minutes"
-                            className="form-control"
-                            required/> 
+                            <input
+                                type="number"
+                                name="cookTime"
+                                onChange={handleChange}
+                                value={formData.cookTime}
+                                placeholder="Cook Time in Minutes"
+                                className="form-control"
+                                required/> 
 
-                        <button>Sumbit</button>
-                    </form>
+                            <button>Sumbit</button>
+                        </form>
+                    </div>
+
+                    <div className="col-4">
+                        <form onSubmit={addToIngredientList}>
+                            <input
+                                type="text"
+                                name="name"
+                                onChange={handleChange}
+                                value={ingredientData.name}
+                                placeholder="Ingredient Name"
+                                className="form-control"
+                                required/>
+
+                            <input 
+                                type="number"
+                                name="amount"
+                                onChange={handleChange}
+                                value={ingredientData.amount}
+                                placeholder="Ingredient Amount"
+                                className="form-control"
+                                required/>
+
+                            <input
+                                type="text"
+                                name="measurement"
+                                onChange={handleChange}
+                                value={ingredientData.measurement}
+                                placeholder="Ingredient Measurement"
+                                className="form-control"
+                                required/>
+
+                            <button>Add Ingredient</button>
+                        </form>
+                        {ingredientList.length ? ingredientList.map(i => 
+                        <NewIngredientDetail ingredient={i}/>) : ""}
+                    </div>
+                    <div className="col-4">
+                        <form onSubmit={addToInstructionList}>
+
+                            <input
+                                type="text"
+                                name="instruction"
+                                onChange={handleChange}
+                                value={instructionData.instructions}
+                                placeholder="Instruction"
+                                className="form-control"
+                                required/>
+
+                            <select
+                                name="optional"
+                                onChange={handleChange}
+                                value={instructionData.optional}
+                                className="form-control"
+                                required>
+                                <option value="" disabled defaultValue>Select your option</option>
+
+                                <option value={true}>Optional</option>
+                                <option value={false}>Madatory</option>   
+                            </select>
+
+                            <button>Add Instruction</button>
+
+                        </form>
+                    </div>
                 </div>
-
-                <div className="row">
-                    <form onSubmit={addToIngredientList}>
-                        <input
-                            type="text"
-                            name="name"
-                            onChange={handleChange}
-                            value={ingredientData.name}
-                            placeholder="Ingredient Name"
-                            className="form-control"
-                            required/>
-
-                        <input 
-                            type="number"
-                            name="amount"
-                            onChange={handleChange}
-                            value={ingredientData.amount}
-                            placeholder="Ingredient Amount"
-                            className="form-control"
-                            required/>
-
-                        <input
-                            type="text"
-                            name="measurement"
-                            onChange={handleChange}
-                            value={ingredientData.measurement}
-                            placeholder="Ingredient Measurement"
-                            className="form-control"
-                            required/>
-
-                        <button>Add Ingredient</button>
-                    </form>
-                </div>
-
-                <div className="row">
-                    <form onSubmit={addToInstructionList}>
-
-                        <input
-                            type="text"
-                            name="instruction"
-                            onChange={handleChange}
-                            value={instructionData.instruction}
-                            placeholder="Instruction"
-                            className="form-control"
-                            required/>
-
-                        <select
-                            name="optional"
-                            onChange={handleChange}
-                            value={instructionData.optional}
-                            className="form-control"
-                            required>
-                            <option value="" disabled defaultValue>Select your option</option>
-
-                            <option value={true}>Optional</option>
-                            <option value={false}>Madatory</option>   
-                        </select>
-
-                        <button>Add Instruction</button>
-
-                    </form>
-                </div>
-{console.log(instructionList)}
             </div>
         )
 };

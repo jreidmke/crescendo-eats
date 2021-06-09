41e87b9a2e5840d9b937d8df849dc6837c3c2ace
James Reid <jreidmke@gmail.com>
2021-06-09 10:16:46 -0500

    resized dish title and added link to dish edit page

--- src/components/recipe-details/DetailsTitle.js
+++ src/components/recipe-details/DetailsTitle.js
@@ -1,7 +1,10 @@
+import { TiEdit } from "react-icons/ti";
+import { Link } from 'react-router-dom';
+
 function DetailsTitle({recipe}) {
     return(
         <div className="col-5 text-wrap" id="dish-title-box">
-            <h1 id="dish-title">{recipe.title}</h1>
+            <h1 id="dish-title">{recipe.title} <Link to={`${recipe.uuid}/edit`}><TiEdit/></Link></h1>
             <h3>{recipe.description}</h3>
         </div>
     )

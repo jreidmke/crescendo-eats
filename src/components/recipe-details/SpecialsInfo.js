74192097faeb461bf8f1d44bc13d2e37126fcab0
James Reid <jreidmke@gmail.com>
2021-06-14 11:57:40 -0500

    refactored to allow for display of multiple specials for single ingredient

--- src/components/recipe-details/SpecialsInfo.js
+++ src/components/recipe-details/SpecialsInfo.js
@@ -10,15 +10,32 @@ import "../../styling/specials-box.css";
 /**Displays specials information. Links to Edit Specials Form. */
 
 function SpecialsInfo(id, specials) {
+    let relaventSpecials = [];
     for(let s of specials) {
-        if(id === s.ingredientId) return(
-            <div className="specials-box">
-                <h6>Special: {s.title.toUpperCase()} ({s.type}) <Link to={`/specials/${s.uuid}/edit`}><FaEdit/></Link></h6>
-                <small>{s.text}</small><br/>
-                {"geo" in s ? <small><a href={`http://maps.google.com/maps?q=${s.geo}`}>Click here for location!</a></small> : ""}
-            </div>
-        );
-    }
+        if(id === s.ingredientId) relaventSpecials.push(s);
+    };
+    return(
+        <div>
+            {relaventSpecials.length ?
+            relaventSpecials.map(s => 
+                <div className="specials-box">
+                    <h6>Special: {s.title.toUpperCase()} ({s.type}) <Link to={`/specials/${s.uuid}/edit`}><FaEdit/></Link></h6>
+                    <small>{s.text}</small><br/>
+                    {"geo" in s ? <small><a href={`http://maps.google.com/maps?q=${s.geo}`}>Click here for location!</a></small> : ""}
+                </div>                
+                )
+            : ""}
+        </div>
+        )
+    // for(let s of specials) {
+    //     if(id === s.ingredientId) return(
+            // <div className="specials-box">
+            //     <h6>Special: {s.title.toUpperCase()} ({s.type}) <Link to={`/specials/${s.uuid}/edit`}><FaEdit/></Link></h6>
+            //     <small>{s.text}</small><br/>
+            //     {"geo" in s ? <small><a href={`http://maps.google.com/maps?q=${s.geo}`}>Click here for location!</a></small> : ""}
+            // </div>
+    //     );
+    // }
 }
 
 export default SpecialsInfo;

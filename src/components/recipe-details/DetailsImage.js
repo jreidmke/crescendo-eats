2118408ab0c352282e94bf52b1041e35af6ffcf8
James Reid <jreidmke@gmail.com>
2021-06-14 11:14:49 -0500

    new doc string and spacing for readability

--- src/components/recipe-details/DetailsImage.js
+++ src/components/recipe-details/DetailsImage.js
@@ -1,9 +1,12 @@
-/**Big image for Recipe Details Page */
+/**Big image for Recipe Details Page. Displays image passed down from RecipeDetails.js or generic image */
 
 function DetailsImage({recipe}) {
     return(
         <div className="col-5" id="dish-image-box">
-            {"images" in recipe ? <img src={`${recipe.images.full}`} className="img-fluid" id="dish-image"/> : <img src={"/img/no_img.jpg"} className="img-fluid" id="dish-image"/>}
+            {"images" in recipe ? 
+                <img src={`${recipe.images.full}`} className="img-fluid" id="dish-image"/> 
+            : 
+                <img src={"/img/no_img.jpg"} className="img-fluid" id="dish-image"/>}
         </div>
     )
 };
